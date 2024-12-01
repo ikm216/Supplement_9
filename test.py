01fca5a28ff77575d66770384a4a1622c933d738
ikm216 <ikmclassof22@gmail.com>
2024-12-01 15:30:28 -0500

    test: added test for integrating a liner function over a specified range

--- test.py
+++ test.py
@@ -1,4 +1,12 @@
-def test():
-    pass
+from scipy.integrate import quad
+from pytest import approx
 
-test
+def test_should_return_quad_linear_function():
+    def linear(x):
+        x = 2 * x + 3
+        return x
+    
+    answer = quad_linear(x, 0, 10)
+    test, _ = quad(x, 0, 10)
+
+    assert answer == pytest.approx(test, rel = 1e - 5)
