b5109f5dc9c5bbfa2a2f9648361f64510ad74b46
ikm216 <ikmclassof22@gmail.com>
2024-12-01 16:37:48 -0500

    doc: added documentation for the feature quad_linear

--- test.py
+++ test.py
@@ -3,6 +3,17 @@ import pytest
 from pytest import approx
 
 def quad_linear(x, beg, end):
+    """
+    Integrates a given linear function over the specified range.
+    
+    Args:
+        x: The linear function to integrate.
+        beg: The start of the integration range.
+        end: The end of the integration range.
+    
+    Returns:
+        The result of the integration.
+    """
     answer, _ = quad(x, beg, end)
     return answer
 
@@ -14,3 +25,5 @@ def test_should_return_quad_linear_function():
     test, _ = quad(linear, 0, 10)
 
     assert answer == pytest.approx(test, rel = 1e-5)
+
+def 
